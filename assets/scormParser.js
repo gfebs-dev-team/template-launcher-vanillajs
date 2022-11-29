26312e4ad316312f87d75071e0a7bbb5c816998d
Rhylen Nguyen <rhylennguyen@gmail.com>
2022-11-29 10:21:26 -0500

    Template Literals

--- assets/scormParser.js
+++ assets/scormParser.js
@@ -1,119 +1,198 @@
 const scorm = require('./scorm.json');
 const fs = require('fs');
-let identifier = scorm.general.course_title.split(" ").map((n)=>n[0]).join("");
+const identifier = scorm.general.course_title.split(" ").map((n) => n[0]).join("");
+const manPath = './imsmanifest.xml';
+const mdPath = scorm.general.course_code + '_course.xml';
 
 /**
  * A function that generates Army SCORM compliant manifest and metadata files
  */
 function generateManifest() {
-    
-    let path = './imsmanifest.xml';
-    let mfData = '<manifest>\n' + generateMetadata() +'\n' + generateOrganizations() + '\n' + generateResources() + '\n</manifest>';
-    fs.writeFile(path, mfData, function (err) {
+    fs.writeFile('./' + mdPath, generateCourseMetadata(), function (err) {
         if (err) throw err;
         console.log(err);
     });
-    console.log(mfData);
+
+    let resources = "";
+
+    let assets = fs.readdirSync('./assets');
+    assets.forEach((asset) => {
+        resources += '\n\t\t<file href="/assets/' + asset + '"/>';
+    })
+
+    let mfData = `<manifest identifier="${identifier}_scorm" version="1"
+    xmlns="http://www.imsglobal.org/xsd/imscp_v1p1"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xmlns:adlcp="http://www.adlnet.org/xsd/adlcp_v1p3"
+    xmlns:adlseq="http://www.adlnet.org/xsd/adlseq_v1p3"
+    xmlns:adlnav="http://www.adlnet.org/xsd/adlnav_v1p3"
+    xmlns:imsss="http://www.imsglobal.org/xsd/imsss"
+    xmlns:lom="http://ltsc.ieee.org/xsd/LOM"
+    xsi:schemaLocation="http://www.imsglobal.org/xsd/imscp_v1p1 imscp_v1p1.xsd
+                        http://www.adlnet.org/xsd/adlcp_v1p3 adlcp_v1p3.xsd
+                        http://www.adlnet.org/xsd/adlseq_v1p3 adlseq_v1p3.xsd
+                        http://www.adlnet.org/xsd/adlnav_v1p3 adlnav_v1p3.xsd
+                        http://www.imsglobal.org/xsd/imsss imsss_v1p0.xsd
+                        http://ltsc.ieee.org/xsd/LOM lom.xsd">
+    <metadata>
+        <schema>ADL SCORM</schema>
+        <schemaversion>2004 3rd Edition</schemaversion>
+        <adlcp:location>${mdPath}_course.xml'</adlcp:location>
+    </metadata>
+    <organizations default="${identifier}">
+        <organization identifier="${identifier}" adlseq:objectivesGlobalToSystem="false">
+            <title>${scorm.general.course_title}</title>
+            <item identifier="${scorm.general.course_code + identifier}" identifierref="${identifier}_SCO">
+                <title>${scorm.general.course_code + ': ' + scorm.general.course_title}</title>
+                <imsss:sequencing>
+                    <imsss:objectives>
+                            <imsss:primaryObjective satisfiedByMeasure="false" objectiveID="scoObj"></imsss:primaryObjective>
+                    </imsss:objectives>
+                </imsss:sequencing>
+            </item>
+            <imsss:sequencing>
+                <imsss:rollupRules>
+                    <imsss:rollupRule childActivitySet="all">
+                        <imsss:rollupConditions conditionCombination="all">
+                            <imsss:rollupCondition condition="satisfied"/>
+                        </imsss:rollupConditions>
+                        <imsss:rollupAction action="completed"/>
+                    </imsss:rollupRule>
+                    <imsss:rollupRule childActivitySet="all">
+                        <imsss:rollupConditions conditionCombination="all">
+                            <imsss:rollupCondition condition="satisfied"/>
+                        </imsss:rollupConditions>
+                        <imsss:rollupAction action="satisfied"/>
+                    </imsss:rollupRule>
+                    <imsss:rollupRule childActivitySet="any">
+                        <imsss:rollupConditions conditionCombination="any">
+                            <imsss:rollupCondition operator="not" condition="satisfied"/></imsss:rollupConditions>
+                        <imsss:rollupAction action="incomplete"/>
+                    </imsss:rollupRule>
+                </imsss:rollupRules>
+            </imsss:sequencing>
+        </organization>
+    </organizations>
+    <resources>
+        <resource identifier="${identifier}_SCO" type="webcontent" adlcp:scormType="sco" href="index.html">${resources}
+        </resource>
+    </resources>
+    </manifest>`
+
+    fs.writeFile(manPath, mfData, function (err) {
+        if (err) throw err;
+        console.log(err);
+    });
 }
 
 generateManifest();
 
-function generateMetadata() {
-    let path = scorm.general.course_code + '_course.xml';
-    fs.writeFile('./' + path, generateCourseMetadata(), function (err) {
-        if (err) throw err;
-        console.log(err);
-      });
-    let metadata = '<metadata>\n\t<schema>ADL SCORM</schema>\n\t<schemaversion>2004 3rd Edition</schemaversion>\n\t<adlcp:location>' + path + '</adlcp:location>\n</metadata>';
-    
-    return metadata;
-}
-
 function generateCourseMetadata() {
-    let rights = '<rights>\n\t<cost>\n\t\t<source>LOMv1.0</source>\n\t\t<value>no</value>\n\t</cost>\n\t<copyrightAndOtherRestrictions>\n\t\t<source>LOMv1.0</source>\n\t\t<value>no</value>/n/t</copyrightAndOtherRestrictions>/n</rights>'
-    let metadata = '<?xml version="1.0"?>\n<lom xmlns="http://ltsc.ieee.org/xsd/LOM" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://ltsc.ieee.org/xsd/LOM lomStrict.xsd">\n' + generateGeneral() + '\n' + generateLifecycle() + '\n' + generateMetaMetadata() + '\n' + generateTechnical() + '\n' + rights + '\n' + generateClassifications() + '\n</lom>';
+    let keywords = "";
 
-    return metadata;
-}
+    scorm.general.keywords.forEach((keyword) => {
+        keywords += '\n\t<keyword><string>' + keyword + '</string></keyword>';
+    });
 
-function generateGeneral() {
-    let general = '<general>\n\t<identifier>\n\t\t<catalog>ATIA</catalog>\n\t\t<entry>TBD</entry>\n\t</identifier>\n\t<title>\n\t\t<string>' + scorm.general.course_title + 
-    '</string>\n\t</title>\n\t<language>' + scorm.general.language + '</language>\n\t<description>\n\t\t<string>' + scorm.general.description + '</string>\n\t</description>'
-    
-    let keywords = scorm.general.keywords;
-
-    keywords.forEach((keyword) => {
-        general += '\n\t<keyword><string>' + keyword + '</string></keyword>';
-    })
-    
-    general += '\n\t<aggregationLevel>\n\t\t<source>LOMv1.0</source>\n\t\t<value>'+ scorm.general.aggregation_level +'</value>\n\t<aggregationLevel>\n</general>'
-
-    //console.log(general);
-    return general;
-}
-
-function generateLifecycle() {
-    let lifeCycle = '<lifeCycle>\n\t<version>\n\t\t<string>' + scorm.lifecycle.version + '</string>\n\t</version>\n\t<status>\n\t\t<source>LOMv1.0</source>\n\t\t<value>' + scorm.lifecycle.status + '</value>\n\t</status>\n\t<contribute>\n\t\t<source>LOMv1.0</source>\n\t\t<value>publisher</value>\n\t\t<entity>\n\t\t\t' + scorm.lifecycle.contribute.entity +'\n\t\t</entity>\n\t\t<date>\n\t\t\t<datetime>' + scorm.lifecycle.contribute.date + '</datetime>\n\t\t</date>\n\t</contribute>\n</lifeCycle>'
-
-    //console.log(lifeCycle);
-    return lifeCycle;
-}
-
-
-function generateMetaMetadata() {
-    let meta = '<metaMetadata>\n\t<identifier>\n\t\t<catalog>TBD</catalog>\n\t\t<entry>TBD</entry>\n\t</identifier>\n\t<metadataSchema>LOMv1.0</metadataSchema>\n\t<metadataSchema>SCORM_CAM_v1.3</metadataSchema>\n\t<metadataSchema>ADLv1.0</metadataSchema>\n\t<language>'+ scorm.general.language +'</language>\n</metaMetadata>'
-
-    //console.log(meta);
-    return meta;
-}
-
-function generateTechnical() {
-    let technical = '<technical>';
+    let technical = '';
     let formats = scorm.technical.format;
 
-    formats.forEach((format)=> {
+    formats.forEach((format) => {
         technical += '\n\t<format>' + format + '</format>'
     })
-
-    technical += '\n</technical>'
     
-    //console.log(technical);
-    return technical;
+    let metadata = `<?xml version="1.0"?>
+    <lom xmlns="http://ltsc.ieee.org/xsd/LOM" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://ltsc.ieee.org/xsd/LOM lomStrict.xsd">
+    <general>
+        <identifier>
+            <catalog>ATIA</catalog>
+            <entry>TBD</entry>
+        </identifier>
+        <title>
+            <string>${scorm.general.course_title }</string>
+        </title>
+        <language>${scorm.general.language}</language>
+        <description>
+            <string>${scorm.general.description}</string>
+        </description>
+        ${keywords}
+        <aggregationLevel>
+            <source>LOMv1.0</source>
+            <value>${scorm.general.aggregation_level}</value>
+        </aggregationLevel>
+    </general>
+    <lifeCycle>
+        <version>
+            <string>${scorm.lifecycle.version}</string>
+        </version>
+        <status>
+            <source>LOMv1.0</source>
+            <value>${scorm.lifecycle.status}</value>
+        </status>
+        <contribute>
+            <role>
+                <source>LOMv1.0</source>
+            <value>publisher</value>
+            </role>
+            <entity>
+                ${scorm.lifecycle.contribute.entity}
+            </entity>
+            <date>
+                <dateTime>${scorm.lifecycle.contribute.date}</dateTime>
+            </date>
+        </contribute>
+    </lifeCycle>
+    <metaMetadata>
+        <identifier>
+            <catalog>TBD</catalog>
+            <entry>TBD</entry>
+        </identifier>
+        <metadataSchema>LOMv1.0</metadataSchema>
+        <metadataSchema>SCORM_CAM_v1.3</metadataSchema>
+        <metadataSchema>ADLv1.0</metadataSchema>
+        <language>${scorm.general.language}</language>
+    </metaMetadata>
+    <technical>${technical}
+    </technical>
+    <rights>
+        <cost>
+            <source>LOMv1.0</source>
+            <value>no</value>
+        </cost>
+        <copyrightAndOtherRestrictions>
+            <source>LOMv1.0</source>
+            <value>no</value>
+        </copyrightAndOtherRestrictions>
+    </rights>
+    ${generateClassifications()}
+    </lom>
+    `
+
+    console.log(metadata);
+    return metadata;
 }
+
 
 function generateClassifications() {
     let classParsed = '';
     let classifications = scorm.classifications;
 
     classifications.forEach((classification) => {
-        classParsed += '<classification>\n\t<purpose>\n\t\t<source>LOMv1.0</source>\n\t\t<value>' + classification.value + '</value>\n\t</purpose>\n\t<description>\n\t\t<string>' + classification.description + '</string>\n\t</description>\n\t<keyword>\n\t\t<string>' + classification.keyword + '</string>\n\t</keyword>\n</classification>\n'
+        classParsed += `<classification>
+        <purpose>
+            <source>LOMv1.0</source>
+            <value>${classification.value}</value>
+        </purpose>
+        <description>
+            <string>${classification.description}</string>
+        </description>
+        <keyword>
+            <string>${classification.keyword}</string>
+        </keyword>
+    </classification>
+    `
     })
 
     //console.log(classParsed)
     return classParsed
-}
-
-
-function generateOrganizations() {
-    const objective = '<imsss:sequencing>\n\t\t\t\t<imsss:objectives>\n\t\t\t\t\t\t<imsss:primaryObjective satisfiedByMeasure="false" objectiveID="scoObj"></imsss:primaryObjective>\n\t\t\t\t</imsss:objectives>\n\t\t\t</imsss:sequencing>';
-    const sequencing = '<imsss:sequencing>\n\t<imsss:rollupRules>\n\t\t<imsss:rollupRule childActivitySet="all">\n\t\t\t<imsss:rollupConditions conditionCombination="all">\n\t\t\t\t<imsss:rollupCondition condition="satisfied"/>\n\t\t\t</imsss:rollupConditions>\n\t\t\t<imsss:rollupAction action="completed"/>\n\t\t</imsss:rollupRule>\n\t\t<imsss:rollupRule childActivitySet="all">\n\t\t\t<imsss:rollupConditions conditionCombination="all">\n\t\t\t\t<imsss:rollupCondition condition="satisfied"/>\n\t\t\t</imsss:rollupConditions>\n\t\t\t<imsss:rollupAction action="satisfied"/>\n\t\t</imsss:rollupRule>\n\t\t<imsss:rollupRule childActivitySet="any">\n\t\t\t<imsss:rollupConditions conditionCombination="any">\n\t\t\t\t<imsss:rollupCondition operator="not" condition="satisfied"/></imsss:rollupConditions>\n\t\t\t<imsss:rollupAction action="incomplete"/>\n\t\t</imsss:rollupRule>\n\t</imsss:rollupRules>\n</imsss:sequencing>';
-
-    let organizations = '<organizations default="'+identifier+'">\n\t<organization identifier="'+ identifier +'" adlseq:objectivesGlobalToSystem="false">\n\t\t<title>'+ scorm.general.course_title +'</title>\n\t\t<item identifier="'+scorm.general.course_code+identifier+'" identifierref="'+scorm.general.course_code+identifier+'_SCO">\n\t\t\t<title>'+scorm.general.course_code+': '+scorm.general.course_title+'</title>\n\t\t\t' + objective + '\n\t\t</item>\n'+ sequencing + '\n\t</organization>\n</organizations>';
-
-    //console.log(organizations);
-    return organizations;
-}
-
-function generateResources() {
-    let resources ='<resources>\n\t<resource identifer="' + identifier + '_SCO" adlcp:scormType="sco" href="index.html">';
-    let assets = fs.readdirSync('./assets');
-
-    assets.forEach((asset) => { 
-        resources += '\n\t\t<file href="/assets/' + asset + '"/>';
-    })
-
-    resources += '\n\t\t<file href="index.html"/>\n\t</resource>\n</resources>';
-
-    //console.log(resources);
-    return resources;
 }
