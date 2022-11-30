fa8968a96469b2d2ab944c06764132fb3c1a3cb4
Rhylen Nguyen <rhylennguyen@gmail.com>
2022-11-30 09:14:06 -0500

    take 5

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,10 +1,9 @@
 <script setup>
-//import { RouterView } from 'vue-router'
-import HomeView from './views/HomeView.vue';
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <HomeView />
+  <RouterView />
 </template>
 
 <style scoped>
--- Vue/vue.config.js
+++ Vue/vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   // options...
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? './' : './'
 })
